9c024a97668131321564f71aea30328baf81e5c1
Arthur Manenti <97462232+noname697@users.noreply.github.com>
2024-10-02 15:02:51 -0300

    Add files via upload

---
+++ js/buscarVideo.js
@@ -0,0 +1,25 @@
+import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
+
+
+async function buscarVideo(evento) {
+    evento.preventDefault()
+
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value
+    const busca = await conectaApi.buscaVideo(dadosDePesquisa)
+    
+    const lista = document.querySelector("[data-lista]")
+
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild)
+    }
+
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+
+    if(busca.length == 0){
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com este termo.</h2>`
+    }
+}
+
+const botaoPesquisa = document.querySelector('[data-botao-pesquisa]')
+botaoPesquisa.addEventListener('click', evento => buscarVideo(evento))
